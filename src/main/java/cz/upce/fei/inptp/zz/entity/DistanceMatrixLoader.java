3419d7db4ee2aec68d0f036eca4c10e373a3d135
Tomáš Křičenský <43739546+st52530@users.noreply.github.com>
2019-10-21 12:20:51 +0200

    Revert "Refactor DistanceMatrixLoader."
    This reverts commit 9d601504781614a97585654f88493d9b1983fee5.

--- src/main/java/cz/upce/fei/inptp/zz/entity/DistanceMatrixLoader.java
+++ src/main/java/cz/upce/fei/inptp/zz/entity/DistanceMatrixLoader.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package cz.upce.fei.inptp.zz.entity;
 
 import java.io.BufferedReader;
@@ -5,6 +10,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -13,20 +20,33 @@ import java.util.Arrays;
 public class DistanceMatrixLoader {
 
     private String fileName;
-    private String columnSeparator = ";";
 
     public DistanceMatrixLoader(String fileName) {
         this.fileName = fileName;
     }
 
     public DistanceMatrix load() {
-        try (
-                FileReader fileReader = new FileReader(fileName);
-                BufferedReader bufferedReader = new BufferedReader(fileReader)
-        ) {
-            String[] locations = loadLocations(bufferedReader);
-            double[][] distances = loadDistances(bufferedReader, locations.length);
+        try (FileReader fileReader = new FileReader(fileName);
+             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
 
+            String header = bufferedReader.readLine();
+            String[] tokens = header.split(";");
+            int numberOfLocations = tokens.length-1;
+            
+            String[] locations = Arrays.copyOfRange(tokens, 1, tokens.length);
+            double[][] distances = new double[numberOfLocations][numberOfLocations];
+            
+            for (int i = 0; i < numberOfLocations; i++) {
+                String loadedLine = bufferedReader.readLine();
+                String[] tokensOnLine = loadedLine.split(";");
+                
+                for (int j = 1; j < tokensOnLine.length; j++) {
+                    double value = Double.parseDouble(tokensOnLine[j]);                    
+                    distances[i][j-1] = value; 
+                }
+                
+            }
+            
             return new DistanceMatrix(locations, distances);
         } catch (FileNotFoundException ex) {
         } catch (IOException ex) {
@@ -34,26 +54,4 @@ public class DistanceMatrixLoader {
         
         return null;
     }
-
-    private String[] loadLocations(BufferedReader reader) throws IOException {
-        String header = reader.readLine();
-        String[] tokens = header.split(columnSeparator);
-        return Arrays.copyOfRange(tokens, 1, tokens.length);
-    }
-
-    private double[][] loadDistances(BufferedReader reader, int numberOfLocations) throws IOException {
-        double[][] distances = new double[numberOfLocations][numberOfLocations];
-
-        for (int i = 0; i < numberOfLocations; i++) {
-            String loadedLine = reader.readLine();
-            String[] tokensOnLine = loadedLine.split(columnSeparator);
-
-            for (int j = 1; j < tokensOnLine.length; j++) {
-                distances[i][j - 1] = Double.parseDouble(tokensOnLine[j]);
-            }
-        }
-
-        return distances;
-    }
-
 }
